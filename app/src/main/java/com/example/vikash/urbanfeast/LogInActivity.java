b5bba01d600f3f38e02d515a538f2aa3721b336c
Vikash Kumar Sharma <vikashhrs@gmail.com>
2016-07-20 04:01:06 -0700

    Basic Design

---
+++ app/src/main/java/com/example/vikash/urbanfeast/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.vikash.urbanfeast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Vikash on 20-Jul-16.
+ */
+public class LogInActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/vikash/urbanfeast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.vikash.urbanfeast;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences=getSharedPreferences("logInStatus", Context.MODE_PRIVATE);
+        String status=sharedPreferences.getString("flagValue","NULL");
+        if(status.matches("CLI") || status.matches("GLI") || status.matches("FLI")){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }else{
+            startActivity(new Intent(getApplicationContext(),LogInActivity.class));
+        }
+    }
+}
